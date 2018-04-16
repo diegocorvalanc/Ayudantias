247743bef38ec2ad61bad560cb52fdcfb5827965
Diego Corvalán <diego.corvalanc@utem.cl>
2018-04-16 19:29:10 -0300

    Subir

--- Semana 2/Propuesto3.c
+++ Semana 2/Propuesto3.c
@@ -4,19 +4,20 @@
 int main()
 {  
     int a,b,c;
-    printf("Ingrese valor de Lado a: "); 
-    scanf("%d",&a);
-    printf("Ingrese valor de Lado b: ");
-    scanf("%d",&b);
-    printf("Ingrese valor de Lado c: ");
-    scanf("%d",&c);
+    printf("Escribir un programa, que permita leer tres números enteros que representan las longitudes de los lados de un triángulo. Indique si es posible formar un triángulo (a=1, b=2y c=500000).";
+    printf("Ingresar valor de lado a: "); 
+    scanf("%i",&a);
+    printf("Ingresar valor de lado b: ");
+    scanf("%i",&b);
+    printf("Ingresar valor de lado c: ");
+    scanf("%i",&c);
     if(c<(a+b) && a<(c+b) && b<(c+a)) // Comparacion de los lados
     { 
-        printf("Es posible generar un triangulo con estas medidas");
+        printf("Es posible generar un triangulo");
     }
     else // Si no se cumple la condicion del if, ocurre lo siguiente... 
     {
-        printf("Medidas invalidas");
+        printf("No es posible generar un triangulo");
     }
     return 0;
 }
