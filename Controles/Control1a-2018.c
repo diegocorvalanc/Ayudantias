355da04b448e34204c4a746712ba72c610c5e1d8
Diego Corvalán <diego.corvalanc@utem.cl>
2018-04-25 21:07:49 -0300

    Control 1 2018

--- Controles/Control1a-2018.c
+++ Controles/Control1a-2018.c
@@ -1,8 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
+/*Escribir la funcion SumaEspecial(num), que recibe un numero entero de 6 digitos y retorna la suma entre los dos primeros digitos y el ultimo digito
+Ejemplo, si num = 123456 debe retornar (12+6)*/
 
-//Escribir la funcion SumaEspecial(num), que recibe un numero entero de 6 digitos y retorna la suma entre los dos primeros digitos y el ultimo digito
-//Ejemplo, si num = 123456 debe retornar (12+6)
+int contadordigitos(int num); // No era necesario para el control 
 
 int sumaespecial(int num)
 {
@@ -14,7 +15,23 @@ int main()
 	int n;
 	printf("Ingrese un numero de 6 digitos: \n");
 	scanf("%d",&n);
-	printf("Su numero original es: %i y su suma especial es %i ",n,sumaespecial(n));
-	
+	if (contadordigitos(n) != 6)
+	{
+		printf("Por favor ingresar un numero de 6 digitos!\n");
+		main();
+	}
+	else
+		printf("Su numero original es: %i y su suma especial es %i ",n,sumaespecial(n));
 	return 0;
+}
+
+int contadordigitos(int num)
+{
+	int contador = 0, aux = num;
+	while (num !=0)
+	{
+		contador++;
+		num = num/10;
+	}
+	return contador;
 }
